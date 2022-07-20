bfdb42335134907bb50ffa1ec9deb207c0464e95
ayeahgodlove <ayeahgodlove5@gmail.com>
2022-07-20 13:23:10 +0100

    feat(registration): Add user registration component

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+// MUI components
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { emptyUser } from 'src/models/User';
+
+// services
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.css']
+  styleUrls: ['./user-registration-form.component.css'],
 })
 export class UserRegistrationFormComponent implements OnInit {
+  @Input() userData = emptyUser;
+  constructor(
+    private dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    private snackBar: MatSnackBar,
+    private fetchApiData: FetchApiDataService,
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe(
+      (result) => {
+        // Logic for a successful user registration goes here! (To be implemented)
+        this.dialogRef.close(); // This will close the modal on success!
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      },
+      (result) => {
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
   }
-
 }
